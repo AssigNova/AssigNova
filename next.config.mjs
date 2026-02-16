77c92d39a94d19fae1df386701b0ca4d873a6df9
Mahammad Shafin <mdshafinvohra@gmail.com>
2026-02-16 18:19:44 +0530

    integrate cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // next.config.js
+  images: {
+    unoptimized: true, // Mandatory unless using a paid external loader (e.g., Cloudinary)
+  },
   /* config options here */
   output: "export",
 };
