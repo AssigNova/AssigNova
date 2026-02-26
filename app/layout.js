13e521bd8695fc45cad9d75b0f57026195eb61ac
javedakhta <javedakhtar.ja999@gmail.com>
2026-02-26 13:22:31 +0530

    google tag manager tag

--- app/layout.js
+++ app/layout.js
@@ -87,10 +87,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <head>
+        {/* Google Tag Manager */}
+<script>
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WWMWMR5S');</script>
+{/* <!-- End Google Tag Manager --> */}
         <link rel="preconnect" href="https://images.unsplash.com" />
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
       </head>
       <body className={`${inter.className} bg-gray-950 text-gray-100`}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWMWMR5S"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{/* <!-- End Google Tag Manager (noscript) --> */}
         <NextTopLoader
           color="#3b82f6"
           initialPosition={0.08}
